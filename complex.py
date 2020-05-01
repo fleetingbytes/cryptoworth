59482989ae9f5f8032ee7a666ac1918daac0eff8
Nagidal <nagidal146@gmail.com>
2020-05-01 07:36:26 +0200

    Order_Book can sum orders to level2

--- complex.py
+++ complex.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
 
 import dataclasses
-import itertools
 import json
 import logging
 import logging.config
 import math
 import pathlib
 # https://github.com/websocket-client/websocket-client
-from collections import OrderedDict
-from typing import List, Mapping, Union
 from websocket import create_connection
 
 #Own modules:
@@ -25,30 +22,21 @@ options = dict()
 options['origin'] = 'https://exchange.blockchain.com'
 url = "wss://ws.prod.blockchain.info/mercury-gateway/v1/ws"
 ws = create_connection(url, **options)
-symbols = ("BTC-EUR", )
-# symbols = ("BTC-EUR", "ETH-EUR", "LTC-EUR")
-messages = [] + \
-           ['{"action": "subscribe", "channel": "l3", "symbol": "' + f"{symbol}" + '"}' for symbol in symbols]
+symbols = ("BTC-USD", "ETH-USD", "LTC-USD", "BTC-EUR", "ETH-EUR", "LTC-EUR")
+messages = ['{"action": "subscribe", "channel": "heartbeat"}'] + \
+           ['{"action": "subscribe", "channel": "symbols"}'] + \
+           ['{"action": "subscribe", "channel": "l3", "symbol": "' + "{}".format(symbol) + '"}' for symbol in symbols] + \
+           ['{"action": "subscribe", "channel": "prices", "symbol": "' + "{}".format(symbol) + '", "granularity": 60}' for symbol in symbols] + \
+           ['{"action": "subscribe", "channel": "ticker", "symbol": "' + "{}".format(symbol) + '"}' for symbol in symbols] + \
+           ['{"action": "subscribe", "channel": "trades", "symbol": "' + "{}".format(symbol) + '"}' for symbol in symbols]
 
 @dataclasses.dataclass
 class Order_Book:
     """
     Holds a list of bids and a list of asks for a particular symbol
     """
-    bids: OrderedDict = dataclasses.field(default_factory=OrderedDict)
-    asks: OrderedDict = dataclasses.field(default_factory=OrderedDict)
-    def sum_orders(self) -> OrderedDict:
-        """
-        outputs bids or asks as level2 (summed to price levels)
-        """
-        for order_type in (self.bids, self.asks):
-            px_qty = OrderedDict()
-            for order in sorted(order_type.values(), reverse=True, key=lambda order: order.px):
-                try:
-                    px_qty[order.px] = px_qty[order.px] + order.qty
-                except KeyError:
-                    px_qty[order.px] = order.qty
-            yield px_qty
+    bids: dict = dataclasses.field(default_factory=dict)
+    asks: dict = dataclasses.field(default_factory=dict)
 
 
 @dataclasses.dataclass(frozen=True)
@@ -64,22 +52,10 @@ class Order:
 @dataclasses.dataclass
 class Wallet:
     name: str
-    dollar: float = 0
-    euro: float = 0
+    dollars: float = 0
     bitcoin: float = 0
     ether: float = 0
     litecoin: float = 0
-
-
-@dataclasses.dataclass
-class Message:
-    seqnum: str = ""
-    event: str = ""
-    channel: str = ""
-    timestamp: str = ""
-    symbol: str = ""
-    bids: List[Mapping[str, Union[str, float, int]]] = dataclasses.field(default_factory=list)
-    asks: List[Mapping[str, Union[str, float, int]]] = dataclasses.field(default_factory=list)
 
 
 class Exchange:
@@ -91,25 +67,20 @@ class Exchange:
     def __init__(self, symbols):
         self.symbols = symbols
         self.order_books = dict(zip((symbol for symbol in self.symbols), (Order_Book() for symbol in self.symbols)))
-    def evaluate(self, w: Wallet):
-        logger.debug(f"Evaluating wallet")
-        for ob in self.order_books.values():
-            bids, asks = ob.sum_orders()
-            breakpoint()
     def process_update(self, message):
-        symbol = message["symbol"]
-        bids = message["bids"]
-        asks = message["asks"]
-        for bid in bids:
+        for bid in message["bids"]:
             if bid["qty"] == 0:
-                del(self.order_books[symbol].bids[bid["id"]])
+                del(self.order_books[message["symbol"]].bids[bid["id"]])
             else:
-                self.order_books[symbol].bids[bid["id"]] = Order(**bid)
-        for ask in asks:
+                self.order_books[message["symbol"]].bids[bid["id"]] = Order(**bid)
+        for ask in message["asks"]:
             if ask["qty"] == 0:
-                del(self.order_books[symbol].asks[ask["id"]])
+                del(self.order_books[message["symbol"]].asks[ask["px"]])
             else:
-                self.order_books[symbol].asks[ask["id"]] = Order(**ask)
+                self.order_books[message["symbol"]].asks[ask["px"]] = Order(**ask)
+        logger.debug(f"{message['symbol']} Bids:")
+        for order in sorted(self.order_books[message["symbol"]].bids.values(), reverse=True, key=lambda order: order.px):
+            logger.debug(f"Price: {order.px}, Quantity: {order.qty}, Id: {order.id}")
     def process_subscribed(self, message):
         logger.debug(f"Subscribed to {message['symbol']}")
     def process_unknown(self, message):
@@ -160,23 +131,14 @@ def wait_key() -> str:
     return str(result, encoding="utf-8").upper()
 
 
-myWallet = Wallet(name="Sven's wallet",
-                  dollar=5.21, 
-                  euro=0.29, 
-                  bitcoin=3.70511573+0.0599833+0.094, 
-                  ether=12.65+0.03, 
-                  litecoin=79.691+0.1)
-
-
 if __name__ == "__main__":
     for message in messages:
         logger.debug(message)
-    exchange = Exchange(symbols)
-    with saver.Saver(pathlib.Path("~/exchange_data")) as svr:
-        try:
-            for message in listen(ws, messages):
-                svr.save(message)
-                exchange.process(message)
-                exchange.evaluate(myWallet)
-        except KeyboardInterrupt:
-            logger.debug("Cought Keyboard Interrupt, quitting.")
+    # exchange = Exchange(symbols)
+    # with saver.Saver(pathlib.Path("~/exchange_data")) as svr:
+        # try:
+            # for message in listen(ws, messages):
+                # svr.save(message)
+            # # exchange.process(message)
+        # except KeyboardInterrupt:
+            # logger.debug("Cought Keyboard Interrupt, quitting.")
--- saver.py
+++ saver.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from __future__ import annotations
+import itertools
+import json
 import logging
 import pathlib
 import datetime
@@ -16,6 +18,7 @@ class Saver:
     Saves messages from the socket for permanent record.
     """
     def __init__(self, path_to_folder: pathlib.Path):
+        self.started = datetime.datetime.now(tz=datetime.timezone.utc)
         self.parent = path_to_folder.expanduser()
         self.heartbeats = self.parent / pathlib.Path("heartbeats")
         self.orders = self.parent / pathlib.Path("orders")
@@ -28,23 +31,80 @@ class Saver:
         self.channels = dict(zip(("heartbeat", "l3", "prices", "symbols", "ticker", "trades"), self.subfolders))
         self.weekdays = dict(zip(range(7), ("SU", "MO", "TU", "WE", "TH", "FR", "SA")))
         self.create_file_paths()
+        self.counter = itertools.count()
+        self.now = datetime.datetime.now(tz=datetime.timezone.utc)
     def __enter__(self) -> Saver:
         logger.debug("Entering Saver")
-        self.hbf = open(self.heartbeats_file_path, mode="wb")
+        self.hbf = open(self.heartbeats_file_path, mode="w")
+        logger.debug(f"Opened {self.hbf.name}")
+        self.syf = open(self.symbols_file_path, mode="w")
+        logger.debug(f"Opened {self.syf.name}")
+        self.pxf = open(self.prices_file_path, mode="w")
+        logger.debug(f"Opened {self.pxf.name}")
+        self.tkf = open(self.ticker_file_path, mode="w")
+        logger.debug(f"Opened {self.tkf.name}")
+        self.trf = open(self.trades_file_path, mode="w")
+        logger.debug(f"Opened {self.trf.name}")
+        self.orf = open(self.orders_file_path, mode="w")
+        logger.debug(f"Opened {self.orf.name}")
         return self
     def __exit__(self, exc_type, exc_value, traceback) -> bool:
+        for fd in (self.hbf, self.syf, self.pxf, self.tkf, self.trf, self.orf):
+            fd.flush()
+            fd.close()
+            logger.debug(f"Saved {fd.name}")
         logger.debug("Leaving Saver")
-        self.hbf.flush()
-        self.hbf.close()
         return False
+    def format_datetime(self, dt: datetime.datetime):
+        return f"{dt.strftime('%Y%m%d')}{self.weekdays[int(dt.strftime('%w'))]}{dt.strftime('%H%M%S')}"
     def create_folders(self, folders: Tuple[pathlib.Path]):
         for folder in folders:
             folder.mkdir(parents=True, exist_ok=True)
     def create_file_paths(self):
-            now = datetime.datetime.now(tz=datetime.timezone.utc)
-            self.heartbeats_file_path = self.heartbeats / f"{now.strftime('%Y%m%d')}{self.weekdays[int(now.strftime('%w'))]}{now.strftime('%H%M%S')}.hb"
+            self.heartbeats_file_path = self.heartbeats / (self.format_datetime(self.started) + ".hb")
+            self.symbols_file_path = self.symbols / (self.format_datetime(self.started) + ".sy")
+            self.prices_file_path = self.prices / (self.format_datetime(self.started) + ".px")
+            self.ticker_file_path = self.ticker / (self.format_datetime(self.started) + ".tk")
+            self.trades_file_path = self.trades / (self.format_datetime(self.started) + ".tr")
+            self.orders_file_path = self.orders / (self.format_datetime(self.started) + ".or")
+    def dump_dict_as_json(self, d: Mapping[str, str], fd: io.BufferedWriter):
+        print(self.format_datetime(datetime.datetime.now(tz=datetime.timezone.utc)), file=fd)
+        json.dump(d, fd, indent=4, separators=(",", ": "))
+        print("\n", file=fd)
+    def save_hb(self, msg: Mapping[str, str]):
+        self.dump_dict_as_json(msg, self.hbf)
+    def save_sy(self, msg: Mapping[str, str]):
+        self.dump_dict_as_json(msg, self.syf)
+    def save_px(self, msg: Mapping[str, str]):
+        self.dump_dict_as_json(msg, self.pxf)
+    def save_tk(self, msg: Mapping[str, str]):
+        self.dump_dict_as_json(msg, self.tkf)
+    def save_tr(self, msg: Mapping[str, str]):
+        self.dump_dict_as_json(msg, self.trf)
+    def save_or(self, msg: Mapping[str, str]):
+        self.dump_dict_as_json(msg, self.orf)
     def save(self, msg):
-        self.hbf.write(str.encode(repr(msg) + "\n", encoding="utf-8"))
+        if msg["channel"] == "l3":
+            save_target = self.save_or
+        elif msg["channel"] == "heartbeat":
+            save_target = self.save_hb
+        elif msg["channel"] == "prices":
+            save_target = self.save_px
+        elif msg["channel"] == "ticker":
+            save_target = self.save_tk
+        elif msg["channel"] == "trades":
+            save_target = self.save_tr
+        elif msg["channel"] == "symbols":
+            save_target = self.save_sy
+        save_target(msg)
+        current_message_count = next(self.counter)
+        self.now = datetime.datetime.now(tz=datetime.timezone.utc)
+        delta = self.now - self.started
+        try:
+            if delta.seconds % 53 == 0:
+                logger.debug(f"Message count: {current_message_count}, {msg['seqnum']}")
+        except AttributeError:
+            pass
 
 
 if __name__ == "__main__":
